1cf26c0a90f03fb1d2af6b77df10ee342546cdc9
Jiwoong <dnd0311@naver.com>
2024-08-07 17:38:04 +0900

    crud finished

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async getProducts() {
+    const products = await this.productRepository.find();
+    return { count: products.length, data: products };
+  }
+
+  async getProduct(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
+    return product;
+  }
+
+  async postProduct(createdProductDto: CreateProductDto) {
+    const newProduct = await this.productRepository.create(createdProductDto);
+    await this.productRepository.save(newProduct);
+    return newProduct;
+  }
+
+  async deleteProducts() {
+    const deleteResponse = await this.productRepository.delete({});
+    if (!deleteResponse) {
+      throw new HttpException('Something new Error', HttpStatus.NOT_FOUND);
+    }
+    return 'Deleted every product';
+  }
+
+  async deleteProductById(id: string) {
+    const deleteResponse = await this.productRepository.delete({ id });
+    if (!deleteResponse) {
+      throw new HttpException('Products not Found', HttpStatus.NOT_FOUND);
+    }
+    return 'Deleted the product';
+  }
+
+  async updateProductById(id: string, createdProductDto: CreateProductDto) {
+    await this.productRepository.update(id, createdProductDto);
+    const updatedProduct = await this.productRepository.findOneBy({ id });
+    if (updatedProduct) {
+      return updatedProduct;
+    }
+    throw new HttpException('Update Denied', HttpStatus.NOT_FOUND);
+  }
+}
