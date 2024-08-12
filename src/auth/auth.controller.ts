31a6555af67410d19b3efdba5bf6e2cd5f4cdbfa
Jiwoong <dnd0311@naver.com>
2024-08-12 17:30:59 +0900

    login process #1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginUserDto } from '../user/dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('/signup')
   async signupUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.signupUser(createUserDto);
+  }
+
+  @Post('/login')
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.loginUser(loginUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginUserDto } from '../user/dto/login-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +10,17 @@ export class AuthService {
 
   async signupUser(createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const user = await this.userService.getUserByEmail(loginUserDto.email);
+    const isMatched = await bcrypt.compare(
+      loginUserDto.password,
+      user.password,
+    );
+    if (!isMatched) {
+      throw new HttpException('Passwords do not match', HttpStatus.BAD_REQUEST);
+    }
+    return user;
   }
 }
