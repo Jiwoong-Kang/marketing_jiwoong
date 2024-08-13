153013e89e18e5bc7516786a3571d3cf3a3640b1
Jiwoong <dnd0311@naver.com>
2024-08-13 09:05:57 +0900

    configservice-> configmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalAuthStrategy } from './strategies/local-auth.strategy';
 import { AccessTokenStrategy } from './strategies/access-token.strategy';
 
 @Module({
-  imports: [UserModule, ConfigService, JwtModule.register({})],
+  imports: [UserModule, ConfigModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, LocalAuthStrategy, AccessTokenStrategy],
 })
