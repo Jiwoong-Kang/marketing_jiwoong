1f0723a9bb5d832bfd686f4201cddb45d8f12db0
Jiwoong <dnd0311@naver.com>
2024-09-08 23:07:04 -0600

    minio #3,4

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,13 +7,20 @@ import {
   Post,
   Put,
   Query,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { PageOptionsDto } from '@common/dtos/page-options.dto';
 import { PageDto } from '@common/dtos/page.dto';
 import { Product } from '@product/entities/product.entity';
+import { BufferedFile } from '@root/minio-client/file.model';
+import { FileInterceptor } from '@nestjs/platform-express';
+import RoleGuard from '@auth/guards/role.guard';
+import { Role } from '@common/enums/role.enum';
 
 @ApiTags('Product')
 @Controller('product')
@@ -33,10 +40,58 @@ export class ProductController {
   }
 
   @Post('/create')
-  async registerProduct(@Body() createProductDto: CreateProductDto) {
-    return await this.productService.postProduct(createProductDto);
-  }
+  @UseInterceptors(FileInterceptor('image'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: CreateProductDto })
+  @ApiBody({
+    description: 'A single image file with additional product data',
+    schema: {
+      type: 'object',
+      properties: {
+        image: {
+          type: 'string',
+          format: 'binary',
+          description: 'Product image file',
+        },
 
+        name: {
+          type: 'string',
+          description: 'Name of the product',
+          example: 'IPhone16',
+        },
+
+        description: {
+          type: 'string',
+          description: 'Description of the product',
+          example: 'This phone is good',
+        },
+
+        price: {
+          type: 'number',
+          description: 'Price of the product',
+          example: '640000',
+        },
+
+        stock: {
+          type: 'number',
+          description: 'Stock of the product',
+          example: '100',
+        },
+
+        category: {
+          type: 'string',
+          description: 'Category of product',
+          example: 'Mobile',
+        },
+      },
+    },
+  })
+  async registerProduct(
+    @UploadedFile() productImg?: BufferedFile,
+    @Body() createProductDto?: CreateProductDto,
+  ) {
+    return await this.productService.postProduct(productImg, createProductDto);
+  }
   @Delete('/delete')
   async deleteAllProducts() {
     return await this.productService.deleteProducts();
@@ -47,11 +102,57 @@ export class ProductController {
     return await this.productService.deleteProductById(id);
   }
 
+  @UseGuards(RoleGuard(Role.ADMIN))
   @Put('/:id')
+  @UseInterceptors(FileInterceptor('image'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: CreateProductDto })
+  @ApiBody({
+    description: 'A single image file with additional product data',
+    schema: {
+      type: 'object',
+      properties: {
+        image: {
+          type: 'string',
+          format: 'binary',
+          description: 'Product image file',
+        },
+
+        name: {
+          type: 'string',
+          description: 'Name of the product',
+          example: 'IPhone',
+        },
+
+        description: {
+          type: 'string',
+          description: 'Description of the product',
+          example: 'This phone works well',
+        },
+
+        price: {
+          type: 'number',
+          description: 'Price of the product',
+          example: '5000',
+        },
+
+        stock: {
+          type: 'number',
+          description: 'Stock of the product',
+          example: '10',
+        },
+      },
+    },
+  })
   async updateProductById(
     @Param('id') id: string,
-    @Body() createProductDto: CreateProductDto,
+    @UploadedFile() productImg?: BufferedFile,
+    @Body() updateProductDto?: CreateProductDto,
   ) {
-    return await this.productService.updateProductById(id, createProductDto);
+    return await this.productService.updateProductById(
+      id,
+      productImg,
+      updateProductDto,
+    );
   }
 }
